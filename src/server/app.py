014b91213ce12d1c02f6bc3b0273365f4ba87879
Unknown <mythologyali@gmail.com>
2018-05-28 04:07:36 -0700

    gitignore and index changes

--- src/server/app.py
+++ src/server/app.py
@@ -1,35 +1,33 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file, url_for, send_from_directory, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from src.server.configuration.env import setup_env
 from src.server.configuration.logger import configure_logger
-from src.server.exceptions.exception import InvalidUsage, NotFound, Unauthorized
-
-
-def create_app(db):
-	app = Flask(__name__)
-	db.app = app
-	db.init_app(app)
-	migrate.init_app(app, db)
-	return app
 
 
 configure_logger()
-migrate = Migrate()
-db = SQLAlchemy()
 
-app = create_app(db)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/api_test'
+app = Flask(__name__, template_folder='templates')
+database_uri, port, host = setup_env()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 # Circular import prevention
+from src.server.exceptions.exception import InvalidUsage, NotFound, Unauthorized
 from src.server.endpoints.client_endpoint import ClientAPI
 from src.server.endpoints.user_endpoint import UserAPI
-from src.server.endpoints.message_endpoint import MessageAPI
+import src.server.endpoints.message_endpoint as message_endpoint
+import src.server.routes.website
+MessageAPI = message_endpoint.MessageAPI
 
-app.add_url_rule('/clients', view_func=ClientAPI.as_view('clients'))
-app.add_url_rule('/users', view_func=UserAPI.as_view('users'))
-app.add_url_rule('/messages', view_func=MessageAPI.as_view('messages'))
+app.add_url_rule('/clients', view_func=ClientAPI.as_view('clients'), subdomain='ap  i')
+app.add_url_rule('/users', view_func=UserAPI.as_view('users'), subdomain='api')
+app.add_url_rule('/messages', view_func=MessageAPI.as_view('messages'), subdomain='api')
 
 
 @app.errorhandler(InvalidUsage)
