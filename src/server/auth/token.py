f9ee7171d1597f0cc5086c1f859b944310b1a215
Unknown <mythologyali@gmail.com>
2018-05-28 04:04:03 -0700

    Added better logging

--- src/server/auth/token.py
+++ src/server/auth/token.py
@@ -1,7 +1,5 @@
 import binascii
 import os
-from functools import wraps
-from src.server.exceptions.exception import Unauthorized
 
 
 def generate_token():
@@ -9,14 +7,3 @@ def generate_token():
 	return binascii.hexlify(os.urandom(12)).decode()
 
 
-def requires_token(func):
-	@wraps(func)
-	def check_token(*args, **kwargs):
-		# Check to see if it's in their session
-		if 'api_token' not in '':
-			# If it isn't return our access denied message (you can also return a redirect or render_template)
-			raise Unauthorized()
-
-		# Otherwise just send them where they wanted to go
-		return func(*args, **kwargs)
-	return check_token
--- src/server/configuration/logger.py
+++ src/server/configuration/logger.py
@@ -1,18 +1,19 @@
 from logging.config import dictConfig
 
+
 def configure_logger():
 	dictConfig({
-	    'version': 1,
-	    'formatters': {'default': {
-	        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-	    }},
-	    'handlers': {'wsgi': {
-	        'class': 'logging.StreamHandler',
-	        'stream': 'ext://flask.logging.wsgi_errors_stream',
-	        'formatter': 'default'
-	    }},
-	    'root': {
-	        'level': 'INFO',
-	        'handlers': ['wsgi']
-	    }
+		'version': 1,
+		'formatters': {'default': {
+			'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+		}},
+		'handlers': {'wsgi': {
+			'class': 'logging.StreamHandler',
+			'stream': 'ext://flask.logging.wsgi_errors_stream',
+			'formatter': 'default'
+		}},
+		'root': {
+			'level': 'INFO',
+			'handlers': ['wsgi']
+		}
 	})
