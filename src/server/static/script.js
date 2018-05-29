8213d10ffeaebf915755c88f71b246d2b76fb076
Unknown <mythologyali@gmail.com>
2018-05-29 06:11:54 -0700

    added new static files

--- src/server/static/script.js
+++ src/server/static/script.js
@@ -1,12 +1,22 @@
 const s  = window.location.href;
 var pageLocation = (s.split('/').pop());
-const color = '#ffc591'
+const color = '#ffc591';
+
+function applyEffect(element){
+    element.style.boxShadow = '0 0 20px #000;';
+    element.style.color = color;
+    element.style.textShadow = '-3px 3px 20px #a99fff, 1px -1px 20px #7b84ff;';
+}
+
+var elem;
 switch (pageLocation) {
     case 'commands':
-        document.getElementById('commands-link').style.color = color;
+        elem = document.getElementById('commands-link');
+        applyEffect(elem);
         break;
     case '':
     case 'home':
-        document.getElementById('home-link').style.color = color;
+        elem = document.getElementById('home-link');
+        applyEffect(elem);
 
 }
