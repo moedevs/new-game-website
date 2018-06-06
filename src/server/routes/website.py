7ffd89b9d243f4b1daa4189fc53e4390ddc110a1
Unknown <mythologyali@gmail.com>
2018-06-05 22:03:02 -0700

    changes to app.py and website routing

--- src/server/routes/website.py
+++ src/server/routes/website.py
@@ -2,17 +2,17 @@ from flask import render_template
 from ..app import app
 
 
-@app.route('/')
+@app.route('/', subdomain='')
 def send_welcome():
 	return render_template('home/index.html')
 
 
-@app.route('/commands', methods=['GET', 'POST'])
+@app.route('/commands', methods=['GET', 'POST'], subdomain='')
 def commands():
 	return render_template('commands/commands.html')
 
 
-@app.route('/home', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'], subdomain='')
 def home():
 	return render_template('home/index.html')
 
