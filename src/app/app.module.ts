a825cf9b4031e7a67b195b0420d34553f8601b54
Unknown <mythologyali@gmail.com>
2018-07-21 15:23:44 -0700

    adding routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {HomeComponent} from './home/home.component';
 import {DropletComponent} from './droplet/droplet.component';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import { HomeHeaderComponent } from './home-header/home-header.component';
 import { CommandsComponent } from './commands/commands.component';
-import {RouterModule, Routes} from "@angular/router";
+import {RouterModule, Routes} from '@angular/router';
 import { CommandDisplayComponent } from './command-display/command-display.component';
-import {MatCardModule} from "@angular/material";
+import {MatCardModule} from '@angular/material';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {UploaderComponent} from './uploader/uploader.component';
 
 const appRoutes: Routes = [
-    {path: 'commands', component: CommandsComponent}
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'commands', component: CommandsComponent},
+    {path: 'uploader', component: UploaderComponent}
 ];
 @NgModule({
     declarations: [
@@ -22,14 +28,17 @@ const appRoutes: Routes = [
         HomeHeaderComponent,
         CommandsComponent,
         CommandDisplayComponent,
+        UploaderComponent,
     ],
     imports: [
         RouterModule.forRoot(
-            appRoutes, {enableTracing: true}
+            appRoutes
         ),
         BrowserModule,
         MatCardModule,
         HttpClientModule,
+        MatTooltipModule,
+        BrowserAnimationsModule,
         NgbModule.forRoot()
     ],
     providers: [],
