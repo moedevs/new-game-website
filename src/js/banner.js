9f1ba7f57748a7a8db1b0394116abfd182a8726d
Xetera <mythologyali@gmail.com>
2019-01-30 15:11:51 -0800

    general layout

---
+++ src/js/banner.js
@@ -0,0 +1,15 @@
+const endpoint = "https://www.reddit.com/r/newgame/about.json";
+const discordEndpoint = "https://discordapp.com/api/v6/invite/ZWW5CJw?with_counts=true";
+const rusers = document.querySelector('#reddit-users');
+const dusers = document.querySelector('#discord-users');
+
+const get = e => fetch(e).then(r => r.json())
+
+get(endpoint)
+  .then(r =>
+    rusers.textContent = r.data.subscribers
+  );
+get(discordEndpoint)
+  .then(r =>
+    dusers.textContent = r.approximate_member_count
+  );
