3269bfd96d6e4e178c892db8883ecf451a6d2dd3
Xetera <mythologyali@gmail.com>
2019-02-01 12:26:34 -0800

    fixed girl panel scaling and alternation

--- src/js/banner.js
+++ src/js/banner.js
@@ -1,10 +1,11 @@
+import { $$, get } from './utils';
+
 const endpoint = "https://www.reddit.com/r/newgame/about.json";
 const discordEndpoint = "https://discordapp.com/api/v6/invite/ZWW5CJw?with_counts=true";
 
-const rusers = document.querySelector('#reddit-users');
-const dusers = document.querySelector('#discord-users');
+const rusers = $$('#reddit-users');
+const dusers = $$('#discord-users');
 
-const get = e => fetch(e).then(r => r.json());
 
 get(endpoint).then(r =>
   rusers.textContent = r.data.subscribers
