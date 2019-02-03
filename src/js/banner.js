141c17b2c91b6cabc1e662081861c13d40b6e768
jvyden420 <jvyden420@gmail.com>
2019-02-03 02:09:50 -0500

    Add semicolons, should fix tests failing

--- src/js/banner.js
+++ src/js/banner.js
@@ -7,8 +7,8 @@ const dusers = $$('#discord-users');
 
 get(endpoint, { mode: "cors" } ).then(r => {
   rusers.textContent = r.subscribers,
-  dusers.textContent = r.discordUsers
+  dusers.textContent = r.discordUsers;
 }).catch(r => {
-  console.log(r)
+  console.log(r);
   //TODO: add backup incase hifumi api is down
 });
