2820b11351784a036f7d181ecaadb58451258e38
jvyden420 <jvyden420@gmail.com>
2019-02-03 01:48:03 -0500

    Change hifumi api host

--- src/js/banner.js
+++ src/js/banner.js
@@ -1,13 +1,14 @@
 import { $$, get } from './utils';
 
-const endpoint = "https://whamer.comxa.com/api/hifumi.php";
+const endpoint = "https://whamer.000webhostapp.com/api/hifumi.php";
 
 const rusers = $$('#reddit-users');
 const dusers = $$('#discord-users');
 
-
 get(endpoint, { mode: "cors" } ).then(r => {
-  //console.log(r),
   rusers.textContent = r.subscribers,
   dusers.textContent = r.discordUsers
+}).catch(r => {
+  console.log(r)
+  //TODO: add backup incase hifumi api is down
 });
