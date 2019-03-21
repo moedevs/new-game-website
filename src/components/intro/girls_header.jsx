5965d8a87a3e4e0cd76fb283ac38c179af9fdd90
Xetera <mythologyali@gmail.com>
2019-03-21 02:00:02 -0700

    linter fix

--- src/components/intro/girls_header.jsx
+++ src/components/intro/girls_header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box } from "bloomer";
 
-import "./girls_header.scss"
+import "./girls_header.scss";
 
 export const GirlsHeader = () =>
   <Box className="girls-header-box has-text-centered is-size-1-desktop is-size-2-tablet is-size-3-mobile">
